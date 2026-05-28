042eaf1ed9d6a673ee0bc41a7fd71dae10b15083
Yoelvis Hernandez <yoelvishernandez86@gmail.com>
2026-05-28 11:05:42 +0200

    cambiando la base de la configuración a "/Modern_art_gallery/"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/spa_and_beauty/",
+  base: "/Modern_art_gallery/",
   build: {
     rollupOptions: {
       input: {
