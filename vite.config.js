c04f4588229e54902e08eaade64623911120411b
Yoelvis Hernandez <yoelvishernandez86@gmail.com>
2026-05-31 11:14:05 +0200

    actualizando la configuración de la base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        location: resolve(__dirname, "location.html"),
+  base: "/spa_and_beauty/",
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
       },
     },
   },
