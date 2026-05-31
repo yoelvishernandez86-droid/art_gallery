fd2ba5eb58e0d8ee2cf01eb0b8e73b9e02d4e7a0
Yoelvis Hernandez <yoelvishernandez86@gmail.com>
2026-05-31 11:19:46 +0200

    actualizando la configuración de la base en vite.config.js 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/spa_and_beauty/",
+  base: "/art_gallery/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
